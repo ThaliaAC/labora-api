b5c7795ad208c41a5914d894611e924b9894fa8a
Thalia Arrascue <thalia.arrascue@gmail.com>
2023-05-18 19:57:38 -0500

    Update Math

--- math_testing/math_test.go
+++ math_testing/math_test.go
@@ -20,11 +20,22 @@ type addTest struct {
 	arg1, arg2, expected int
 }
 
+type subtractTest struct {
+	arg1, arg2, expected int
+}
+
 var addTests = []addTest{
 	{2, 3, 5},
 	{4, 8, 12},
 	{6, 9, 15},
 	{3, 10, 13},
+}
+
+var subtractTests = []subtractTest{
+	{3, 2, 1},
+	{8, 4, 4},
+	{9, 3, 6},
+	{10, 3, 7},
 }
 
 func TestAdd(t *testing.T) {
@@ -45,4 +56,23 @@ func BenchmarkAdd(b *testing.B) {
 func ExampleAdd() {
 	fmt.Println(Add(4, 6))
 	// Output: 10
+}
+
+func TestSubtract(t *testing.T) {
+	for _, test := range subtractTests {
+		if output := Subtract(test.arg1, test.arg2); output != test.expected {
+			t.Errorf("Output %q not equal to expected %q", output, test.expected)
+		}
+	}
+}
+
+func BenchmarkSubtract(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Subtract(15, 10)
+	}
+}
+
+func ExampleSubtract() {
+	fmt.Println(Subtract(15, 10))
+	// Output: 5
 }
